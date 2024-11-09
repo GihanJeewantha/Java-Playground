d3afa99c84c8efeb442386335aaa2a34c81e6fb7
Jeewantha Dilshan <106438829+GihanJeewantha@users.noreply.github.com>
2024-11-09 23:10:21 +0530

    Challenge 1: Print "Hello, World!"

---
+++ Challenge 1 Print HelloWorld/src/Main.java
@@ -0,0 +1,15 @@
+
+/*Objective: Write a Java program that prints "Hello, World!" to the console.
+
+Steps:
+Create a new Java file named HelloWorld.java.
+Write code to display the text "Hello, World!" on the screen.
+Run the program to see the output.*/
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello World!");
+
+    }
+}
