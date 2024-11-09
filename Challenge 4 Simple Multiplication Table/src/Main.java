744263c6ea7efa5353c58d143bd6048f74be095e
Jeewantha Dilshan <106438829+GihanJeewantha@users.noreply.github.com>
2024-11-10 00:41:54 +0530

    Challenge 4: Simple Multiplication Table

---
+++ Challenge 4 Simple Multiplication Table/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Generate multiplication table
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
